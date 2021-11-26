bc7e4920c282539046793c7b2d12f1947ada2a66
Jonathan Montes Romero <jmontes8686@gmail.com>
2021-11-25 18:38:51 -0800

    started to work on frontend handlebars

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
 
-routers.use('/users', userRoutes)
+
+router.use('/users', userRoutes)
 
 module.exports = router;
