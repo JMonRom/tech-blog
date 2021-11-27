ba17882138d8d13bc8fdcb0ce67ca0bef50edfa8
Jonathan Montes Romero <jmontes8686@gmail.com>
2021-11-26 17:42:53 -0800

    finished user model and added for comment and post

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,10 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-// const {  }
+// const {} = require('../models');
+
+
+// router.get('/', (req, res) => {
+
+// })
 
 module.exports = router;
