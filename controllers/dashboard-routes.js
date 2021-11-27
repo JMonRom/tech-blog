08ab0da51957b2e0788db579ccf82a0da9173fbb
Jonathan Montes Romero <jmontes8686@gmail.com>
2021-11-26 20:33:20 -0800

    started working on hbs to correct error sent back

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+
+// get user all posts
+router.get('/', withAuth, (req, res) => {
+  Post.findAll({
+    where: {
+      user_id: req.session.user_id
+    },
+    attributes: ['id', 'title', 'post_txt', 'create_at'],
+    order: [[]]
+  })
+})
+
+
+module.exports = router;
