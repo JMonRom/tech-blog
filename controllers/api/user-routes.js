e3221bcf12a189751fe10220cd14bc060b0bfbdd
Jonathan Montes Romero <jmontes8686@gmail.com>
2021-11-29 16:49:01 -0800

    finished edit and delete button functions changed some things from the add function made to work with edit/delete

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -90,5 +90,37 @@ router.post('/logout', (req, res) => {
   }
 });
 
+router.put('/:id', (req, res) => {
+  User.update(req.body, {
+    indivudalHooks: true,
+    where: {
+      id: req.params.id
+    }
+  }).then(dbUserData => {
+    if(!dbUserData[0]) {
+      res.status(404).json({ message: 'No user found using this ID'});
+      return;
+    }
+    res.json(dbUserData);
+  }).catch(err => {
+    res.status(500).json(err);
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  User.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(404).json({ message: 'No uder found with this ID'});
+      return;
+    }res.json(dbUserData);
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
 
 module.exports = router;
