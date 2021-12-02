44634943afb142e2d31f344c842b1a61404a018a
Jonathan Montes Romero <jmontes8686@gmail.com>
2021-12-02 12:45:21 -0800

    trying to fix heroku errors

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
-const path = require('path');
 const express = require('express');
-const session = require('express-session');
+const sequelize = require('./config/connection');
+const path = require('path');
+const routes = require('./controllers');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const helpers = require('./utils/helpers');
+
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-
+//setup session
 const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
+  secret: 'supersecretsessionsecrettext',
+  cookie: { maxAge: 180000},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
@@ -21,20 +23,20 @@ const sess = {
   })
 };
 
-app.use(session(sess));
-
+//handlebars initialization
 const hbs = exphbs.create({ helpers });
-
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('./controllers/'));
+app.use(session(sess));
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-  sequelize.sync({ force: false });
+//use routes
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now Listening on ${PORT}`));
 });
