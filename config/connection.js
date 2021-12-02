abe5a145e4638581e20b9535e8d8d4e0cd82caee
Jonathan Montes Romero <jmontes8686@gmail.com>
2021-12-02 14:25:02 -0800

    heroku failed

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,5 @@
 const Sequelize = require('sequelize');
+
 require('dotenv').config();
 
 let sequelize;
@@ -6,14 +7,11 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306,
-    },
-  );
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  });
 }
+
+module.exports = sequelize;
